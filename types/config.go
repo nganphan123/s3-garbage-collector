f98e2e32f81c9a4ce8c8032811059f3ef7bfd04d
nganphan123 <jill01009@gmail.com>
2025-06-28 03:26:03 -0700

    remove pointers

--- types/config.go
+++ types/config.go
@@ -1,17 +1,17 @@
 package types
 
 type DeleteConfig struct {
-	ApiVersion string      `json:"apiVersion,omitEmpty"`
-	Kind       string      `json:"kind,omitEmpty"`
-	Selectors  *[]Selector `json:"selectors"`
+	ApiVersion string     `json:"apiVersion,omitEmpty"`
+	Kind       string     `json:"kind,omitEmpty"`
+	Selectors  []Selector `json:"selectors"`
 }
 
 type Selector struct {
-	MatchExpression string             `json:"matchExpression,omitEmpty"`
-	Tags            *map[string]string `json:"tags"`
-	LastAccess      *LastAccess        `json:"lastAccess"`
-	S3ObjMetaData   *map[string]any    `json:"objMetadata"`
-	WithoutTagKeys  *[]string          `json:"withoutTagKeys"`
+	MatchExpression string            `json:"matchExpression,omitEmpty"`
+	Tags            map[string]string `json:"tags"`
+	LastAccess      *LastAccess       `json:"lastAccess"`
+	S3ObjMetaData   map[string]any    `json:"objMetadata"`
+	WithoutTagKeys  []string          `json:"withoutTagKeys"`
 }
 
 type LastAccess struct {
