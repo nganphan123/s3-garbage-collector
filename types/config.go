d0111916d03fe11bc10da7d8dc9589839cca06b7
nganphan123 <jill01009@gmail.com>
2025-06-29 23:34:14 -0700

    add field optional

--- types/config.go
+++ types/config.go
@@ -3,15 +3,15 @@ package types
 type DeleteConfig struct {
 	ApiVersion string     `json:"apiVersion,omitempty"`
 	Kind       string     `json:"kind,omitempty"`
-	Selectors  []Selector `json:"selectors"`
+	Selectors  []Selector `json:"selectors,omitempty"`
 }
 
 type Selector struct {
 	MatchExpression string            `json:"matchExpression,omitempty"`
-	Tags            map[string]string `json:"tags"`
-	LastAccess      *LastAccess       `json:"lastAccess"`
-	S3ObjMetaData   map[string]any    `json:"objMetadata"`
-	WithoutTagKeys  []string          `json:"withoutTagKeys"`
+	Tags            map[string]string `json:"tags,omitempty"`
+	LastAccess      *LastAccess       `json:"lastAccess,omitempty"`
+	S3ObjMetaData   map[string]string `json:"objMetadata,omitempty"`
+	WithoutTagKeys  []string          `json:"withoutTagKeys,omitempty"`
 }
 
 type LastAccess struct {
